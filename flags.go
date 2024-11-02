98ab3566f0767e7c3e47c0875d2a825410e34b19
Eshan Gupta <connectwithcuriouscoding@gmail.com>
2024-11-02 15:27:57 +0000

    updated flag description for AOF

--- flags.go
+++ flags.go
@@ -12,7 +12,7 @@ type flags struct {
 
 func getFlags() flags {
 	flg := &flags{}
-	flag.StringVar(&flg.persistence, "persistence", "inmemory", "What kind of persistence should be used: inmemory, snapshot?")
+	flag.StringVar(&flg.persistence, "persistence", "inmemory", "What kind of persistence should be used: inmemory, snapshot, aof?")
 	flag.DurationVar(&flg.snapshotInterval, "snapshot-interval", 5*time.Second, "Snapshot interval time")
 	flag.Parse()
 	return *flg
