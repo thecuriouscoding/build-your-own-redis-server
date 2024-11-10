1637440f632b54c8f5a89f7e190fbd3435e03ca6
Eshan Gupta <connectwithcuriouscoding@gmail.com>
2024-11-10 09:37:24 +0000

    SET, GET and DEL handlers added

--- common.go
+++ common.go
@@ -1,5 +1,15 @@
 package main
 
+type valueFormat struct {
+	ValueType string
+	Value     interface{}
+}
+
 var (
-	handlers = make(map[string]func(args []string) string)
+	handlers  = make(map[string]func(args []string) string)
+	dataStore = make(map[string]valueFormat)
+)
+
+var (
+	VALUE_TYPE_STRING = "string"
 )
--- handlers.go
+++ handlers.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // command handler gets called on initial COMMAND command sent from redis-cli like client
 func addCOMMANDHandler() {
 	commandHandler := func(args []string) string {
@@ -8,6 +10,53 @@ func addCOMMANDHandler() {
 	handlers["COMMAND"] = commandHandler
 }
 
+func addSETHandler() {
+	// SET [key] [value]
+	setHandler := func(args []string) string {
+		key := args[0]
+		value := args[1]
+		dataStore[key] = valueFormat{
+			ValueType: VALUE_TYPE_STRING,
+			Value:     value,
+		}
+		return "+OK\r\n"
+	}
+	handlers["SET"] = setHandler
+}
+
+func addGETHandler() {
+	// GET [key]
+	getHandler := func(args []string) string {
+		key := args[0]
+		if val, ok := dataStore[key]; !ok {
+			return "$-1\r\n"
+		} else {
+			return fmt.Sprintf("$%d\r\n%s\r\n", len(val.Value.(string)), val.Value.(string))
+		}
+	}
+	handlers["GET"] = getHandler
+}
+
+func addDELHandler() {
+	// DEL [keys...]
+	delHandler := func(args []string) string {
+		var totalDeleted = 0
+		for _, key := range args {
+			if _, ok := dataStore[key]; !ok {
+				continue
+			} else {
+				totalDeleted++
+				delete(dataStore, key)
+			}
+		}
+		return fmt.Sprintf(":%d\r\n", totalDeleted)
+	}
+	handlers["DEL"] = delHandler
+}
+
 func addBasicHandlers() {
 	addCOMMANDHandler()
+	addSETHandler()
+	addGETHandler()
+	addDELHandler()
 }
